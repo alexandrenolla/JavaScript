8257a5493561007c7de21463d22df3e19dc92ef7
Alexandre H F Nolla <alexandre.nolla@gmail.com>
2022-07-25 19:20:55 -0300

    50% do curso concluído. Avante!

---
+++ ex008.js
@@ -0,0 +1,7 @@
+var vel = 80
+console.log (`A sua velocidade é de ${vel}Km/h.`)
+
+if (vel > 60) {
+    console.log ('Sua velocidade está acima do permitido. Você foi MULTADO!')
+}
+console.log ('Use sempre cinto de segurança!')
